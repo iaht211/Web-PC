cfa76eae44d5696085a4532fec6cbd4855977fd6
thai <123443192+iaht211@users.noreply.github.com>
2024-06-24 22:12:42 +0700

    #36 set up mysql with spring

--- src/main/java/com/vn/laptopshop/HelloController.java
+++ src/main/java/com/vn/laptopshop/HelloController.java
@@ -1,6 +1,5 @@
 package com.vn.laptopshop;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Hello world";
+        return "Hello world update";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "Only user can use this link";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "Only admin can use this link";
     }
 }
