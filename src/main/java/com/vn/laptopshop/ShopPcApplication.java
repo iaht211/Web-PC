f2c0135bf51fea6fb1b905e11b2d28d5fc794070
thai <thainv215135.vnist@gmail.com>
2024-06-24 20:40:41 +0700

    init project

---
+++ src/main/java/com/vn/laptopshop/ShopPcApplication.java
@@ -0,0 +1,13 @@
+package com.vn.laptopshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopPcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopPcApplication.class, args);
+	}
+
+}
