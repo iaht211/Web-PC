cc927ce822183285c053f26b52cb94e11820cad6
thai <123443192+iaht211@users.noreply.github.com>
2024-07-09 16:41:34 +0700

    #95 design client homepage

--- src/main/java/com/vn/laptopshop/config/WebMvcConfig.java
+++ src/main/java/com/vn/laptopshop/config/WebMvcConfig.java
@@ -38,5 +38,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
         registry.addResourceHandler("/js/**")
                 .addResourceLocations("/resources/js/");
+        registry.addResourceHandler("/client/**")
+                .addResourceLocations("/resources/client/");
     }
 }
